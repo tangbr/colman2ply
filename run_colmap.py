a2928102205f4f28b0dc2662da2e85a7034be1bc
Xiao Ming Tang <xmtang1@myseneca.ca>
2025-04-06 00:24:54 -0400

    run_colmap.py got init func for DB

--- run_colmap.py
+++ run_colmap.py
@@ -2,16 +2,25 @@ import subprocess
 import argparse
 import os
 
+def initialize_database(database_path):
+    subprocess.run([
+        "colmap", "database_creator",
+        "--database_path", database_path
+    ], check=True)
+
 def run_colmap(image_dir, output_dir):
     os.makedirs(output_dir, exist_ok=True)
-
+    # Initialize database
+    database_path = f"{output_dir}/database.db"
+    initialize_database(database_path)
     # 1. Feature extraction
     subprocess.run([
         "colmap", "feature_extractor",
         "--database_path", os.path.join(output_dir, "database.db"),
         "--image_path", image_dir,
         "--ImageReader.single_camera", "1"
-        "--SiftExtraction.use_gpu", "0"  
+        "--SiftExtraction.use_gpu", "0"
+        "--verbose"   
     ], check=True)
 
     # 2. Exhaustive matching
